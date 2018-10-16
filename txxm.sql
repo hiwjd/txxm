3d0fb5727d1f84dde0e103901da985115541b96d
hiwjd <hiwjd0@gmail.com>
2018-10-16 09:49:59 +0800

    add missing sql

---
+++ txxm.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE `txxm` DEFAULT CHARACTER SET = `utf8mb4`;
+USE `txxm`;
+
+-- Create syntax for TABLE 'operator'
+CREATE TABLE `operator` (
+  `uid` int(11) unsigned NOT NULL,
+  `un` varchar(30) NOT NULL DEFAULT '',
+  `mobile` varchar(20) NOT NULL DEFAULT '',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Create syntax for TABLE 'user'
+CREATE TABLE `user` (
+  `uid` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `un` varchar(30) NOT NULL DEFAULT '',
+  `password` varchar(255) NOT NULL DEFAULT '',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
